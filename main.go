0294cc130dab5572f301c636d8bfdbde9e6f8f98
Erin Krengel <ekrengel@pulumi.com>
2020-01-31 14:09:50 -0800

    add per person and per milestone info, cleanup

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"flag"
+	"fmt"
 	"log"
 	"os"
 
@@ -11,9 +12,19 @@ import (
 	"golang.org/x/oauth2"
 )
 
+// Issue contains the information for a GitHub issue that we care about for planning purposes.
+type Issue struct {
+	Issue     string
+	Milestone string
+	Assignee  string
+	Points    int
+	URL       string
+}
+
 var (
 	flagGitHubOrganization = flag.String("org", "default", "GitHub organization to scan.")
 	flagGitHubLabel        = flag.String("label", "default", "GitHub label marking all issues to be included.")
+	flagAll                = flag.Bool("all", false, "Prints all issues in one table")
 )
 
 func main() {
@@ -40,7 +51,6 @@ func main() {
 	tc := oauth2.NewClient(ctx, ts)
 	client := github.NewClient(tc)
 
-	var sumPoints int
 	// https://developer.github.com/v3/issues/#list-issues
 	opts := &github.IssueListOptions{
 		ListOptions: github.ListOptions{
@@ -50,9 +60,7 @@ func main() {
 		State:  "open",
 		Labels: []string{*flagGitHubLabel},
 	}
-	t := table.NewWriter()
-	t.SetOutputMirror(os.Stdout)
-	t.AppendHeader(table.Row{"Issue", "Milestone", "Assignee", "Points", "URL"})
+	var allIssues []Issue
 
 	for {
 		issues, resp, err := client.Issues.ListByOrg(ctx, *flagGitHubOrganization, opts)
@@ -61,20 +69,7 @@ func main() {
 		}
 
 		for _, i := range issues {
-			points := getSizeValue(i)
-			sumPoints += points
-
-			var milestoneStr string
-			if milestone := i.GetMilestone(); milestone != nil {
-				milestoneStr = milestone.GetTitle()
-			}
-
-			var assigneeStr string
-			if assignee := i.GetAssignee(); assignee != nil {
-				assigneeStr = assignee.GetLogin()
-			}
-
-			t.AppendRow([]interface{}{i.GetTitle(), milestoneStr, assigneeStr, points, i.GetHTMLURL()})
+			allIssues = append(allIssues, createIssue(i))
 		}
 
 		// Fetch the next page of results as needed.
@@ -83,9 +78,34 @@ func main() {
 		}
 		opts.Page = resp.NextPage
 	}
-	t.AppendFooter(table.Row{"", "", "Total", sumPoints})
-	t.AppendFooter(table.Row{"", "", "Avg Per Milestone", sumPoints / 3.0})
-	t.Render()
+	// We only print the all issues view, if the all flag was specified.
+	if flagAll != nil && *flagAll {
+		renderTable(allIssues, "All Issues")
+		return
+	}
+
+	renderPerAssignee(allIssues)
+}
+
+func createIssue(gitHubIssue *github.Issue) Issue {
+	points := getSizeValue(gitHubIssue)
+
+	var milestoneStr string
+	if milestone := gitHubIssue.GetMilestone(); milestone != nil {
+		milestoneStr = milestone.GetTitle()
+	}
+
+	var assigneeStr string
+	if assignee := gitHubIssue.GetAssignee(); assignee != nil {
+		assigneeStr = assignee.GetLogin()
+	}
+	return Issue{
+		Issue:     gitHubIssue.GetTitle(),
+		Milestone: milestoneStr,
+		Assignee:  assigneeStr,
+		Points:    points,
+		URL:       gitHubIssue.GetHTMLURL(),
+	}
 }
 
 func getSizeValue(issue *github.Issue) int {
@@ -104,4 +124,43 @@ func getSizeValue(issue *github.Issue) int {
 	// The issue does not have a size label. We will return a high value to
 	// make sure we realize something is up.
 	return 500
+}
+
+func renderTable(issues []Issue, headerText string) {
+	pointsPerMilestone := make(map[string]int)
+
+	t := table.NewWriter()
+	t.SetOutputMirror(os.Stdout)
+	t.AppendHeader(table.Row{headerText})
+	t.AppendHeader(table.Row{"Issue", "Milestone", "Assignee", "Points", "URL"})
+	var sumPoints int
+	for _, i := range issues {
+		sumPoints += i.Points
+		t.AppendRow([]interface{}{i.Issue, i.Milestone, i.Assignee, i.Points, i.URL})
+		pointsPerMilestone[i.Milestone] += i.Points
+	}
+
+	t.AppendFooter(table.Row{"", "", "Total", sumPoints})
+
+	for milestone, points := range pointsPerMilestone {
+		milestoneText := fmt.Sprintf("Points for %s", milestone)
+		t.AppendFooter(table.Row{"", "", milestoneText, points})
+	}
+	t.Render()
+}
+
+func renderPerAssignee(issues []Issue) {
+	issuesByAssignee := make(map[string][]Issue)
+
+	for _, i := range issues {
+		if val, ok := issuesByAssignee[i.Assignee]; ok {
+			issuesByAssignee[i.Assignee] = append(val, i)
+		} else {
+			issuesByAssignee[i.Assignee] = []Issue{i}
+		}
+	}
+
+	for assignee, assignedIssues := range issuesByAssignee {
+		renderTable(assignedIssues, fmt.Sprintf("Issues for %s", assignee))
+	}
 }
